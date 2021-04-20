ffca04a6d20ccd009b968d9835238745e5b4be10
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-20 20:51:03 -0300

    add users table migration

---
+++ src/database/migrations/1618962221318-UsersTable.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class UsersTable1618962221318 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "Users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("Users");
+  }
+}
