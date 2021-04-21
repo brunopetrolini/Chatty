03d724f853e80770f8f06ec0d480fc568307d23b
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-21 19:21:51 -0300

    update messages table migration

--- src/database/migrations/1618962681918-MessagesTable.ts
+++ src/database/migrations/1618962681918-MessagesTable.ts
@@ -14,6 +14,7 @@ export class MessagesTable1618962681918 implements MigrationInterface {
           {
             name: "admin_id",
             type: "uuid",
+            isNullable: true,
           },
           {
             name: "user_id",
