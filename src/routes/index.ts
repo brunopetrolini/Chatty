9a1a314393d17e2d9cea9f452d522ce128836bc1
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-21 19:10:26 -0300

    add users route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { settingsRoutes } from "./settings.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/settings", settingsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
