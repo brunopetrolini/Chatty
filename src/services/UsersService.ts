4664f0ca5ae801484d0f586afc6aa1d2457208f7
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-21 18:48:05 -0300

    add UsersService

---
+++ src/services/UsersService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from "typeorm";
+
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  async create(email: string): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = usersRepository.create({ email });
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UsersService };
