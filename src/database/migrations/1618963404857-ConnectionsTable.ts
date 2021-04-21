52ccfc5ff9efd8bba2734e4b5ba39cad337f346c
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-20 21:08:16 -0300

    add connections table migration

---
+++ src/database/migrations/1618963404857-ConnectionsTable.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class ConnectionsTable1618963404857 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "Connections",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "admin_id",
+            type: "uuid",
+            isNullable: true,
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKConnectionUser",
+            referencedTableName: "Users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("Connections");
+  }
+}
