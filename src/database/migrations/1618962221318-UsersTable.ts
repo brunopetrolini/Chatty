8577cb17dfb6c44cfc478ea688f1d1f1825196b0
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-20 21:17:49 -0300

    update users table migration

--- src/database/migrations/1618962221318-UsersTable.ts
+++ src/database/migrations/1618962221318-UsersTable.ts
@@ -4,7 +4,7 @@ export class UsersTable1618962221318 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "Users",
+        name: "users",
         columns: [
           {
             name: "id",
@@ -26,6 +26,6 @@ export class UsersTable1618962221318 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("Users");
+    await queryRunner.dropTable("users");
   }
 }
