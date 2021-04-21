56aa93d18cdc8441ea9a48e9feb9ce45a9217a67
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-20 23:04:21 -0300

    add SettingsController

--- src/routes/settings.routes.ts
+++ src/routes/settings.routes.ts
@@ -1,23 +1,10 @@
-import { Request, Response, Router } from "express";
-import { getCustomRepository } from "typeorm";
+import { Router } from "express";
 
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsController } from "../controllers/SettingsController";
 
 const settingsRoutes = Router();
 
-settingsRoutes.post("/settings", (request: Request, response: Response) => {
-  const { chat, username } = request.body;
-
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const settings = settingsRepository.create({
-    chat,
-    username,
-  });
-
-  settingsRepository.save(settings);
-
-  return response.status(201).send();
-});
+const settingsController = new SettingsController();
+settingsRoutes.post("/", settingsController.handle);
 
 export { settingsRoutes };
