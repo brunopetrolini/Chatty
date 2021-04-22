3a3d588c40ccefceccf9f61b9be9e244135fa9cb
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-22 16:26:28 -0300

    update settings routes

--- src/routes/settings.routes.ts
+++ src/routes/settings.routes.ts
@@ -5,6 +5,8 @@ import { SettingsController } from "../controllers/SettingsController";
 const settingsRoutes = Router();
 
 const settingsController = new SettingsController();
+
 settingsRoutes.post("/", settingsController.create);
+settingsRoutes.get("/:username", settingsController.findByUsername);
 
 export { settingsRoutes };
