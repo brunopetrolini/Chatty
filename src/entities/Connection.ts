3297ee75cd8da1b9ad0bc0dcd74b61655611811a
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-22 15:13:53 -0300

    add Connection entity

---
+++ src/entities/Connection.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { User } from "./User";
+
+@Entity("connections")
+class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  user_id: string;
+
+  @JoinColumn({ name: "user_id" })
+  @OneToOne(() => User)
+  user: User;
+
+  @Column()
+  socket_io: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidv4();
+  }
+}
+
+export { Connection };
