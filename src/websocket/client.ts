b7b550695ba0942e589c9c895362d12eff5a336e
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-23 19:43:27 -0300

    add messages render on client

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -45,5 +45,9 @@ ioServer.on("connect", (socket: Socket) => {
     }
 
     await messagesService.create({ text, user_id });
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit("client_list_all_messages", allMessages);
   });
 });
