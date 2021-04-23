6cd05ad7710bc2fc664daa3ee71ddb74805805a3
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-23 20:43:19 -0300

    add admin.ts

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -2,11 +2,39 @@ import { Socket } from "socket.io";
 
 import { ioServer } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
+import { MessagesService } from "../services/MessagesService";
 
 ioServer.on("connect", async (socket: Socket) => {
   const connectionsService = new ConnectionsService();
+  const messagesService = new MessagesService();
 
   const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
 
   ioServer.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+
+  socket.on("admin_list_messages_by_user", async (params, callback) => {
+    const { user_id } = params;
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    callback(allMessages);
+  });
+
+  socket.on("admin_send_message", async (params) => {
+    const { user_id, text } = params;
+
+    await messagesService.create({
+      text,
+      user_id,
+      admin_id: socket.id,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { socket_id }: any = await connectionsService.findUserId(user_id);
+
+    ioServer.to(socket_id).emit("admin_send_to_client", {
+      text,
+      socket_id: socket.id,
+    });
+  });
 });
